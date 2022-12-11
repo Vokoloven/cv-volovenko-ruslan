1a1d9f5349d1ee89f6fad7527ea66d0dba635037
Vokoloven <48805990+Vokoloven@users.noreply.github.com>
2022-12-11 17:02:58 +0000

    Deploying to gh-pages from @ Vokoloven/cv-volovenko-ruslan@d6935b2610f408d0a4b34b89fcb23c42f1655b77 🚀

--- static/js/709.4a74e80d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[709],{9709:function(e,t,i){i.r(t),i.d(t,{educationList:function(){return a}});var a=[{title:"IT School GoIT (Kyiv)",dateDue:"\u25cf July 2022 - up to now",direction:"\u25cf Fullstack Developer"},{title:"Naval Academy of P.S. Nachimov",dateDue:"\u25cf Specialist of Navigation & Engineering",direction:"\u25cf Ukraine 2015"}]}}]);
-//# sourceMappingURL=709.4a74e80d.chunk.js.map
