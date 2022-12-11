7401aa8d1949b8f7b878732007e63eb5f5110aa4
Ruslan Volovenko <vokoloven@gmail.com>
2022-12-11 19:34:04 +0200

    Deleted marks

--- src/components/Education/educationList.js
+++ src/components/Education/educationList.js
@@ -1,12 +1,12 @@
 export const educationList = [
   {
     title: 'IT School GoIT (Kyiv)',
-    dateDue: '● July 2022 - up to now',
+    dateDue: ' July 2022 - up to now',
     direction: '● Fullstack Developer',
   },
   {
     title: 'Naval Academy of P.S. Nachimov',
-    dateDue: '● Ukraine 2015',
+    dateDue: ' Ukraine 2015',
     direction: '● Specialist of Navigation & Engineering',
   },
 ];
